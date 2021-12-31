e86100e121d4ecef0a0691f85ef33d09e96eb038
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2021-12-31 04:54:29 -0600

    feature(int_literals): Add user-defined literals for fixed-sized integers.

---
+++ include/arche/int_literals.hpp
@@ -0,0 +1,25 @@
+#ifndef ARCHE_INT_LITERALS_HPP_
+#define ARCHE_INT_LITERALS_HPP_
+
+#include <cstdint>
+
+namespace arche::int_literals {
+#define INT_LITERAL(TYPE, SUFFIX) \
+TYPE operator "" SUFFIX(unsigned long long int value) { \
+    return static_cast<TYPE>(value); \
+}
+
+INT_LITERAL(std::int8_t, _i8)
+INT_LITERAL(std::int16_t, _i16)
+INT_LITERAL(std::int32_t, _i32)
+INT_LITERAL(std::int64_t, _i64)
+
+INT_LITERAL(std::uint8_t, _u8)
+INT_LITERAL(std::uint16_t, _u16)
+INT_LITERAL(std::uint32_t, _u32)
+INT_LITERAL(std::uint64_t, _u64)
+
+#undef INT_LITERAL
+}  // namespace arche::int_literals
+
+#endif  // ARCHE_INT_LITERALS_HPP_
